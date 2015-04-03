0dead79dcc01add3bdd4906e13546fa40a5a55a6
xiaolu <rebornwhu@gmail.com>
2015-04-02 23:10:10 -0700

    Check in dictionary

--- Ch30Stockz/main.m
+++ Ch30Stockz/main.m
@@ -10,8 +10,32 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        NSMutableArray *stocks = [[NSMutableArray alloc] init];
+        
+        NSMutableDictionary *stock;
+        stock = [NSMutableDictionary dictionary];
+        [stock setObject:@"AAPL"
+                  forKey:@"symbol"];
+        [stock setObject:[NSNumber numberWithInt:200]
+                  forKey:@"shares"];
+        [stocks addObject:stock];
+        
+        stock = [NSMutableDictionary dictionary];
+        [stock setObject:@"GOOG"
+                  forKey:@"symbol"];
+        [stock setObject:[NSNumber numberWithInt:160]
+                  forKey:@"shares"];
+        [stocks addObject:stock];
+        
+        [stocks writeToFile:@"/tmp/stocks.plist"
+                 atomically:YES];
+        
+        NSArray *stockList = [NSArray arrayWithContentsOfFile:@"/tmp/stocks.plist"];
+        
+        for (NSDictionary *d in stockList) {
+            NSLog(@"I have %@ shares of %@", [d objectForKey:@"shares"], [d objectForKey:@"symbol"]);
+        }
     }
     return 0;
 }
