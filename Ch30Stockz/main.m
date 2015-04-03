5c038db5b959ac72b1f81beccfe2136f1f0e7cba
xiaolu <rebornwhu@gmail.com>
2015-04-02 22:19:10 -0700

    First commit

---
+++ Ch30Stockz/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Ch30Stockz
+//
+//  Created by Xiao Lu on 4/2/15.
+//  Copyright (c) 2015 Xiao Lu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
